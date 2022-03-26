af9a5da9820eabe2509b305b7620d6941dc5f4d5
Mark Southon <mrsouthon@gmail.com>
2022-03-26 12:34:28 +0800

    config for Cloud Run Debugger - edited report-check-app.py and requirements.txt

--- report-check-app.py
+++ report-check-app.py
@@ -1,4 +1,12 @@
 from app import app
 
 if __name__ == "__main__":
+    try:
+      import googleclouddebugger
+      googleclouddebugger.enable(
+        breakpoint_enable_canary=True
+      )
+
+    except ImportError:
+      pass
     app.run(host='0.0.0.0', port=80)
